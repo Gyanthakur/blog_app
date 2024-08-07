e5c0d23d996fff65d29f143f8ff807976e32c466
Gyanthakur <gps.96169@gmail.com>
2024-08-08 00:02:54 +0530

    feat: added navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,31 +1,43 @@
 import Link from "next/link";
-
+import styles from "./links.module.css";
+import NavLink from "./navLink'/navLink";
 const Links = () => {
+	const links = [
+		{
+			title: "Homepage",
+			path: "/",
+		},
+		{
+			title: "About",
+			path: "/about",
+		},
+		{
+			title: "Contact",
+			path: "/contact",
+		},
+		{
+			title: "Blog",
+			path: "/blog",
+		},
+	];
 
-    const links = [
-        {
-            title: "Homepage",
-            path: "/",
-        },
-        {
-            title: "About",
-            path: "/about",
-        },
-        {
-            title: "Contact",
-            path: "/contact",
-        },
-        {
-            title: "Blog",
-            path: "/blog",
-        },
-    ]
-  return (
-    <div classNam e="links">
-      {links.map((link => (
-        <Link href={link.path} key={link.title}>{link.title}</Link>
-      )))}
-    </div>
-  );
+	const session = true;
+	const isAdmin = true;
+
+	return (
+		<div className={styles.links}>
+			{links.map((link) => (
+				<NavLink key={link.title} item={link} />
+			))}
+			{session ? (
+				<>
+					{isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+					<button className={styles.logout}>Logout</button>
+				</>
+			) : (
+				<NavLink item={{ title: "Login", path: "/login" }} />
+			)}
+		</div>
+	);
 };
 export default Links;
