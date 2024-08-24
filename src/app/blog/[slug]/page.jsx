d8acdc3e80175c4430209d5f28dc34b957e7d64f
Gyanthakur <gps.96169@gmail.com>
2024-08-25 01:18:13 +0530

    feat: post desc added

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,35 +1,54 @@
-
 import Image from "next/image";
-import styles from "./singlePost.module.css"
+import styles from "./singlePost.module.css";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
 
-const SinglePostPage = () => {
-    return (
-      <div className={styles.container}>
-        <div className={styles.imgContainer}>
-        <Image className={styles.img} src="/contact.png" alt="img err post" fill/>
-        </div>
+const getData = async (slug) => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>Title</h1>
-          <div className={styles.detail}>
-          <Image className={styles.avatar} src="/gyanim.jpg" alt="img err post" height={50} width={50}/>
+	if (!res.ok) {
+		throw new Error("Something went wrong");
+	}
+	return res.json();
+};
 
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>Gyan thakur</span>
-          </div>
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>01.01.2024</span>
-          </div>
-          </div>
+const SinglePostPage = async ({ params }) => {
+	const { slug } = params;
+	const post = await getData(slug);
+	return (
+		<div className={styles.container}>
+			<div className={styles.imgContainer}>
+				<Image
+					className={styles.img}
+					src="/contact.png"
+					alt="img err post"
+					fill
+				/>
+			</div>
 
-          <div className={styles.content}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quia nisi, quaerat provident exercitationem assumenda minus, saepe obcaecati voluptatem suscipit animi deleniti id aliquid quo possimus. Assumenda ex dolor dolores?
-          </div>
-        </div>
-        
-      </div>
-    );
-  };
-  export default SinglePostPage;
+			<div className={styles.textContainer}>
+				<h1 className={styles.title}>{post.title}</h1>
+				<div className={styles.detail}>
+					<Image
+						className={styles.avatar}
+						src="/gyanim.jpg"
+						alt="img err post"
+						height={50}
+						width={50}
+					/>
+					<Suspense fallback={<div>Loading...</div>}>
+						<PostUser userId={post.userId} />
+					</Suspense>
+
+					<div className={styles.detailText}>
+						<span className={styles.detailTitle}>Published</span>
+						<span className={styles.detailValue}>01.01.2024</span>
+					</div>
+				</div>
+
+				<div className={styles.content}>{post.body}</div>
+			</div>
+		</div>
+	);
+};
+export default SinglePostPage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,38 +1,27 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css"
-const BlogPage = () => {
+
+
+const getData =  async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts",{next: {revalidate: 10 }})
+
+    if(!res.ok){
+        throw new Error("Something went wrong")
+    }
+    return res.json();
+}
+const BlogPage = async () => {
+
+    const posts = await getData();
     return (
       <div className={styles.container}>
-        <div className={styles.post}>
-            <PostCard/>
+        {posts.map((post)=>(
+
+            <div className={styles.post} key={post.id}>
+            <PostCard post={post}/>
         </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
-        <div className={styles.post}>
-            <PostCard/>
-        </div>
+        
+        ))}
        
       </div>
     );
